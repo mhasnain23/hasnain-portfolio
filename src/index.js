95b34fb06d3e56a9f4225007eadaceef3870d53d
mhasnain23 <tryfrelancer23@gmail.com>
2024-04-17 08:22:40 -0700

    amazing bro

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const typed = new Typed('#element', {
+    strings: ['Web Developer', 'UI UX Designer', 'Frontend Developer'],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 1000,
+    loop: true,
+})
+
+let userInfo = document.querySelectorAll("send");
